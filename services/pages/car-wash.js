30abbef510d17f5bca30e2864530bf89b90b402f
KhaosFountain <g00383507@atu.ie>
2022-11-28 12:20:27 +0000

    this contains some of the css for curves designs on the main page

--- services/pages/car-wash.js
+++ services/pages/car-wash.js
@@ -1,3 +1,31 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+
 export default function home(){
-    return <h1>This is the carwashing site</h1>
+    return(
+        <div className={styles.section}>
+        <section className={styles.flexSection1}>
+          <h2>Car wash</h2>
+          <p>This is the car wash section here we will contain link to other page about this topic</p>
+          <div className={styles.curve}></div>
+        </section>
+
+        <section className={styles.flexSection2}>
+          <h2>Power Washing</h2>
+          <p>This is the Power Washing section here we will contain link to other page about this topic</p>
+        </section>
+
+        <section className={styles.flexSection3}> 
+          <h2>Electrician</h2>
+          <p>This is the Electrician section here we will contain link to other page about this topic</p>
+        </section>
+
+        <section className={styles.flexSectio4}>
+          <h2>Plumber</h2>
+          <p>This is the Plumber section here we will contain link to other page about this topic</p>
+        </section>
+      </div>
+    )
 }
--- services/pages/index.js
+++ services/pages/index.js
@@ -1,56 +1,48 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <div className={styles.head}>
-      <h1>
-        Welcome to SERVICES PAGE 
+        <h1>
+          Welcome to SERVICES PAGE
           {/* will change to the correct title later on */}
-      </h1>
+        </h1>
       </div>
 
-      <main className={styles.main}>
+      {/* <main className={styles.main}>
 
         <div className={styles.grid}>
           <a href="http://localhost:3000/car-wash" className={styles.card}>
             <h2>Car wash </h2>
           </a>
+        </div> 
+      </main> */}
 
-          <a href="http://localhost:3000/power-washing" className={styles.card}>
-            <h2>Power Washing </h2>
-          </a>
+      <div className={styles.section}>
+        <section className={styles.flexSection1}>
+          <h2>Car wash</h2>
+          <p>This is the car wash section here we will contain link to other page about this topic</p>
+          <div className={styles.curve}></div>
+        </section>
 
-          <a
-            href="http://localhost:3000/electrician"className={styles.card}>
-            <h2>Electrician</h2>
-          </a>
+        <section className={styles.flexSection2}>
+          <h2>Power Washing</h2>
+          <p>This is the Power Washing section here we will contain link to other page about this topic</p>
+        </section>
 
-          <a
-            href="http://localhost:3000/plumber"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Plumber</h2>
-          </a>
-        </div>
-      </main>
+        <section className={styles.flexSection3}> 
+          <h2>Electrician</h2>
+          <p>This is the Electrician section here we will contain link to other page about this topic</p>
+        </section>
 
-      {/* <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer> */}
+        <section className={styles.flexSectio4}>
+          <h2>Plumber</h2>
+          <p>This is the Plumber section here we will contain link to other page about this topic</p>
+        </section>
+      </div>
     </div>
-  )
+  );
 }
