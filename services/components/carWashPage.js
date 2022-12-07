baa90aa7acfa095607767e07a1ea2f1fa9545565
OISIN O ROURKE - STUDENT <G00379850@atu.ie>
2022-12-07 12:15:43 +0000

    oisin sending code

--- services/components/carWashPage.js
+++ services/components/carWashPage.js
@@ -3,16 +3,61 @@ import Image from "next/image";
 import carValetImage from "../styles/car-valet-cork.jpg";
 import Link from "next/link";
 
+import NavBar from "../components/navBar";
+import navBar from "../components/navBar";
+
 export default function Car() {
   return (
+    <section>
+      <section className={styles.homeBar}>
+        <button className={styles.homeBarBtn} >
+          <Link href="javascript:history.back()"> Home</Link>
+          </button>
+        <div className={styles.homeBarText}>
+          <p>this is the nav bar</p>
+        </div>
 
-    <section className = {styles.valetpage}>
-      <h2>Car valet main Page Title</h2>
+        <button className={styles.homeBarBtn}></button>
+        <button className={styles.homeBarBtn}></button>
+      </section>
 
-      <div className = {styles.valetbox}>
-      <Image src = {carValetImage} className = {styles.valetPageImage}/>
-      
-      </div>
+      <section className={styles.pricePage}>
+        <h2>Car valet main Page Title</h2>
+
+        <div className={styles.priceBox}>
+          <div className={styles.priceOpt}>
+            <Image src={carValetImage} className={styles.pricePageImage} />
+            <h4>
+              <b>John Doe</b>
+            </h4>
+            <p>Architect & Engineer</p>
+          </div>
+
+          <div className={styles.priceOpt}>
+            <Image src={carValetImage} className={styles.pricePageImage} />
+            <h4>
+              <b>John Doe</b>
+            </h4>
+            <p>Architect & Engineer</p>
+          </div>
+
+          <div className={styles.priceOpt}>
+            <Image src={carValetImage} className={styles.pricePageImage} />
+            <h4>
+              <b>John Doe</b>
+            </h4>
+            <p>Architect & Engineer</p>
+          </div>
+
+          <div className={styles.priceOpt}>
+            <Image src={carValetImage} className={styles.pricePageImage} />
+            <h4>
+              <b>John Doe</b>
+            </h4>
+            <p>Architect & Engineer</p>
+          </div>
+        </div>
+      </section>
     </section>
   );
 }
